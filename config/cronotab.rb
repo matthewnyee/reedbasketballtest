6908c09aaa7bb2ff344e73dcebfd40292fb7d074
Matt Yee <mattyee@Matts-MacBook-Pro.local>
2015-09-26 10:25:08 -0700

    more testing of crono jobs

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -23,9 +23,9 @@ end
 # Crono.perform(TestJob).every 100.seconds
 Crono.perform(ResetrsvpJob).every 1.week, on: :sunday, at: {hour:00, min: 05}
 Crono.perform(ResetrsvpJob).every 1.week, on: :sunday
-Crono.perform(ResetrsvpJob).every 1.week, on: :saturday, at: {hour:10, min: 16}
+Crono.perform(ResetrsvpJob).every 1.week, on: :saturday, at: {hour:10, min: 27}
 # Crono.perform(TestemailJob).every 10.seconds
-Crono.perform(ReminderEmailJob).every 1.week, on: :saturday, at: {hour: 10, min: 16}
+Crono.perform(ReminderEmailJob).every 1.week, on: :saturday, at: {hour: 10, min: 27}
 Crono.perform(ReminderEmailJob).every 1.week, on: :saturday
 Crono.perform(ReminderEmailJob).every 1.week, on: :sunday
 Crono.perform(ReminderEmailJob).every 1.week, on: :sunday, at: {hour: 00, min: 05}
